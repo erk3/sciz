96adb63190f414b3e4faf838cad02c65b65cc2af
erk3 <3186136+erk3@users.noreply.github.com>
2018-05-23 16:50:27 +0200

    Fail to purge a maildir is no longer an error but a warning

--- modules/mail_walker.py
+++ modules/mail_walker.py
@@ -39,7 +39,7 @@ class MailWalker:
                 if last_modified_date < ago:
                     os.remove(dir_path + os.sep + f)
         except (OSError, IOError) as e:
-            sg.logger.error('Fail to purge %s mail directory! (Error: %s)' % (dir_path, str(e), ))
+            sg.logger.warning('Fail to purge %s mail directory! (Error: %s)' % (dir_path, str(e), ))
             pass
 
     # Main MailDir walker
