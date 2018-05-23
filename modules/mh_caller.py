44bb500b28f645f3d44aac610192cca6e10cdf30
erk3 <3186136+erk3@users.noreply.github.com>
2018-05-23 16:48:52 +0200

    Fixed the previous fix not fixing the bad data pulling from MH ftp

--- modules/mh_caller.py
+++ modules/mh_caller.py
@@ -119,8 +119,8 @@ class MHCaller:
         mh_r = requests.get("http://%s/%s" % (self.ftpURL, self.ftpTrolls2, ))
         lines = mh_r.text.split('\n')
         for troll in trolls:
-            line = [s for s in lines if (str(troll.id) + ';') in s]
-            if line is not None:
+            line = [s for s in lines if s.startswith(str(troll.id) + ';')]
+            if line is not None and len(line) > 0:
                 id, troll.nom, troll.race, troll.niv, troll.nb_kill, troll.nb_mort, troll.nb_mouche, troll.id_guilde, troll.rang_guilde, troll.etat, troll.intangible, troll.pnj, troll.ami_mh, troll.inscription, troll.blason_url, empty = line[0].split(';')
                 troll.etat = troll.etat != u'1'
                 troll.intangible = troll.intangible != u'0'
