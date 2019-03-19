d115f2e3f998c7595935276ae8b9038a09c20884
erk3 <>
2019-03-19 08:48:07 +0100

    Fixed TP

--- classes/lieu_portail.py
+++ classes/lieu_portail.py
@@ -35,19 +35,3 @@ class Portail(Lieu):
         'polymorphic_identity': 'Portail',
         'inherit_condition': id == Lieu.id
     }
-
-    @hybrid_property
-    def last_seen_at(self):
-        if all(at is None for at in [self.last_sv2_update_at, self.creation_datetime]): return None
-        elif self.last_sv2_update_at is None: return self.creation_datetime
-        elif self.creation_datetime is None: return self.last_sv2_update_at
-        return max(self.last_sv2_update_at, self.creation_datetime)
-
-    @hybrid_property
-    def last_seen_by(self):
-        if all(at is None for at in [self.last_sv2_update_by, self.owner_id]): return None
-        elif self.last_sv2_update_at is None: return self.owner_id
-        elif self.creation_datetime is None: return self.last_sv2_update_by
-        else:
-            if self.last_sv2_update_at > self.creation_datetime: return self.last_sv2_update_by
-            return self.owner_id
